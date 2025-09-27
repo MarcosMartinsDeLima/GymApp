2ed839434740d9b9ed8204dd8734e4720145ffc7
MarcosMartinsDeLima <0timetraveler@gmail.com>
2025-09-27 01:47:16 -0300

    feat : cria entidades base

---
+++ Gym/Models/Exercicio.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace Gym.Models
+{
+    public class Exercicio
+    {
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+        public int IdTreino { get; set; }
+        public string? Nome { get; set; }
+        public string? Series { get; set; }
+        public string? Peso { get; set; }
+    }
+}
---
+++ Gym/Models/Treino.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace Gym.Models
+{
+    public class Treino
+    {
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+        public string? Letra { get; set; }
+        public string? Grupo { get; set; }
+
+        [Ignore]
+        public List<Exercicio> Exercicios { get; set; }
+    }
+}
